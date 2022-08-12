4b298e88319c32bd52ef9d20739e09dab1e03c19
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-08-12 00:47:57 -0300

    🎨 style: adding styles to the Main component for the input and button

--- components/main/styles.ts
+++ components/main/styles.ts
@@ -1,3 +1,60 @@
 import styled from "styled-components";
 
-export const ContainerMain = styled.main``;
+export const ContainerMain = styled.main`
+  display: flex;
+`;
+
+export const ContentMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: -1.65rem auto 0;
+
+  width: 48rem;
+  padding: 0 1rem;
+
+  .add-new-task {
+    display: flex;
+    gap: 8px;
+
+    input {
+      padding: 1rem;
+      width: 39.975rem;
+      max-width: 39.975rem;
+
+      border-radius: 8px;
+      outline: none;
+
+      border: 1px solid var(--gray-700);
+      color: var(--gray-300);
+      background: var(--gray-500);
+
+      &::placeholder {
+        color: var(--gray-300);
+      }
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      padding: 1rem;
+
+      font-size: 0.875rem;
+      font-weight: 700;
+
+      border-radius: 8px;
+      border: none;
+
+      cursor: pointer;
+
+      color: var(--gray-100);
+      background: var(--blue-700);
+
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.9);
+      }
+    }
+  }
+`;

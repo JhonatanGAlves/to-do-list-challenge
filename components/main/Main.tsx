b35eb009716d2ffa1c9fed833b608b47a4ecc794
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-08-12 00:46:53 -0300

    feat: populing the Main component with the input and button

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-import { ContainerMain } from "./styles";
+import { ContainerMain, ContentMain } from "./styles";
+import { PlusCircle } from "phosphor-react";
 
 export const Main = () => {
-  return <ContainerMain></ContainerMain>;
+  return (
+    <ContainerMain>
+      <ContentMain>
+        <div className="add-new-task">
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button>
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </div>
+      </ContentMain>
+    </ContainerMain>
+  );
 };

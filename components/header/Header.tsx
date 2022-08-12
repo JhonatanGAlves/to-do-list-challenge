dac3044f526713db8791f0f97cd977faff666cc2
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-08-12 00:06:10 -0300

    👨🏽‍💻 feat: populing the Header component with the values and ending it

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { ContainerHeader } from "./styles";
+import rocketLogo from "../../assets/images/rocket-logo.svg";
 
 export const Header = () => {
-  return <ContainerHeader></ContainerHeader>;
+  return (
+    <ContainerHeader>
+      <a href="/">
+        <img src={rocketLogo} alt="Logo do cabeçalho" />
+      </a>
+      <strong>
+        to<span>do</span>
+      </strong>
+    </ContainerHeader>
+  );
 };

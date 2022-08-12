1391ebf5444f5b6ffae142023e95f89f7a7525e2
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-08-12 01:28:59 -0300

    👨🏽‍💻 feat: populing the Main component with to-do-list section without tasks

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { ContainerMain, ContentMain } from "./styles";
 import { PlusCircle } from "phosphor-react";
+import clipboardIcon from "../../assets/images/clipboard-icon.svg";
 
 export const Main = () => {
   return (
@@ -12,6 +12,26 @@ export const Main = () => {
             Criar
             <PlusCircle size={16} />
           </button>
+        </div>
+        <div className="tasks-created-and-completed">
+          <div className="tasks-created">
+            <strong>Tarefas criadas</strong>
+            <div className="counter">0</div>
+          </div>
+          <div className="tasks-completed">
+            <strong>Tarefas criadas</strong>
+            <div className="counter">0</div>
+          </div>
+        </div>
+        <div className="to-do-list">
+          <div className="no-tasks">
+            <img src={clipboardIcon} alt="Ícone de Clipboard" />
+            <p>
+              Você ainda não tem tarefas cadastradas
+              <br />
+              <span>Crie tarefas e organize seus itens a fazer</span>
+            </p>
+          </div>
         </div>
       </ContentMain>
     </ContainerMain>
